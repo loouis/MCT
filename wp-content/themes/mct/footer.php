475e250741431deaf8c8de5ab54bff2f4cd73940
Louis Smith <louis@ahoia.com>
2016-02-15 15:27:46 +0000

    Edit google tag condition for pages

--- wp-content/themes/mct/footer.php
+++ wp-content/themes/mct/footer.php
@@ -118,7 +118,7 @@
 <script>(function(d) {var config = {kitId: 'rrd4otj',scriptTimeout: 3000},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);</script>
 
 
-<?php if ( is_page(array('home', 'blog', 'locations') ) ){ ?>
+<?php if ( is_page(5) || is_page(113) || is_page(7) ){ ?>
 
 	<!-- Google Code for Remarketing Tag -->
 	<script type="text/javascript">
